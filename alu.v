6bc4c61f6ebad9788baafc3a238184f204a89cb8
LadePrahlad <ladeprahlad2005@gmail.com>
2025-07-08 22:24:07 +0530

    Add files via upload

---
+++ alu.v
@@ -0,0 +1,69 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 08.07.2025 15:07:38
+// Design Name: 
+// Module Name: alu
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module alu(
+    input [7:0] x,
+    input [7:0] y,
+    input [3:0] sel,
+    output reg [7:0] out,
+    output reg zero,
+    output reg carry,
+    output reg overflow,
+    output reg negative
+    );
+    parameter Add = 0, Sub = 1,
+              MUL = 2, AND = 3,
+              OR = 4, XOR = 5,XNOR=6,
+                NOT = 7, NAND =8,
+               NOR= 9, SLT = 10,
+              SLL = 11, SLR= 12,
+              ROL = 13, ROR = 14;
+    always @(*)begin
+    carry=0;overflow=0;
+    case(sel)
+    Add: begin
+    {carry,out}=x+y;
+    overflow=(~x[7]&~y[7]&out[7])|(x[7]&y[7]&~out[7]);
+    end
+    Sub:begin  
+    {carry,out}=x-y;
+    overflow=(x[7]& ~y[7]& ~out[7]) | (~x[7] & y[7] & out[7]);
+    end
+    MUL:out=x*y;
+    AND:out=x&y;
+    OR:out=x|y;
+    XOR:out=x^y;
+    XNOR:out=~(x^y);
+    NOT:out=~x;
+    NAND:out=~(x & y);
+    NOR:out=(~x|y);
+    SLT:out=(x<y)?8'd1:8'd0;
+    SLL:out=x<<1;
+    SLR:out=x>>1;
+    ROL:out={x[6:0],x[7]};
+    ROR:out={x[0],x[7:1]};
+    default:out=8'd0;
+    endcase
+    negative=out[7];
+     zero=(out==8'd0)?1'b1:1'b0;
+     end
+endmodule
---
+++ alu_tb.v
@@ -0,0 +1,71 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 08.07.2025 15:22:34
+// Design Name: 
+// Module Name: alu_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module alu_tb();
+ reg  [7:0] x,y;
+ reg  [3:0] sel;
+ wire [7:0] out;
+ wire zero,carry,overflow,negative;
+ alu uut(x,y,sel,out,zero,carry,overflow,negative);
+ initial begin
+ $display("ALU Testbench Starts");
+ $monitor("x=%d y=%d sel=%b -> out=%d zero=%b carry=%b overflow=%b negative=%b", x, y, sel, out, zero,carry,overflow,negative);
+ x=8'd25;y=8'd15;
+ sel=0;
+ #10 sel=1;
+ #10 sel=2;
+ #10 sel=3;
+ #10 sel=4;
+ #10 sel=5;
+ #10 sel=6;
+ #10 sel=7;
+ #10 sel=8;
+ #10 sel=9;
+ #10 sel=10;
+ #10 sel=11;
+ #10 sel=12;
+ #10 sel=13;
+ #10 sel=14;
+ 
+ #10 x=8'd128;y=8'd128;
+ sel=0;
+ #10 sel=1;
+ #10 sel=2;
+ #10 sel=3;
+ #10 sel=4;
+ #10 sel=5;
+ #10 sel=6;
+ #10 sel=7;
+ #10 sel=8;
+ #10 sel=9;
+ #10 sel=10;
+ #10 sel=11;
+ #10 sel=12;
+ #10 sel=13;
+ #10 sel=14;
+ $finish;
+ end
+ initial begin
+    $dumpfile("wave.vcd");    // This tells the simulator to generate the file
+    $dumpvars(0, alu_tb);     // This dumps all variables in alu_tb
+end
+endmodule
